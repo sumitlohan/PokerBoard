49e3b3e081c728977f8c1e558175400f9fb8bab3
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-12 15:38:48 +0530

    PR review

---
+++ main/constants.js
@@ -0,0 +1,4 @@
+const BASE_URL = "http://localhost:8000/api/";
+angular.module("pokerPlanner").constant("config", {
+  BASE_URL
+});
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname));
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
