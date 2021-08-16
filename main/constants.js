3ff329c8f15685c12f3c1be2f10251bf067878db
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-16 17:04:49 +0530

    APP_CONSTANTS

--- main/constants.js
+++ main/constants.js
@@ -1,6 +1,6 @@
 "use strict";
 (function () {
-  angular.module("pokerPlanner").constant("config", {
+  angular.module("pokerPlanner").constant("APP_CONSTANTS", {
     BASE_URL: config.BASE_URL,
   });
 })();
