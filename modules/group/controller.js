2e13b3dee919f0b7924c2435fa71d2d52d5af17a
Rohit Jain <rohit.jain@joshtechnologygroup.com>
2021-08-25 14:41:51 +0530

    tab

--- modules/group/controller.js
+++ modules/group/controller.js
@@ -1,24 +1,24 @@
 'use strict';
 (function () {
-	angular.module('pokerPlanner').controller('groupCtrl', [
-		'$scope', '$stateParams', 'groupService',
-		function ($scope, $stateParams, groupService) {
-			const id = $stateParams.id;
-			$scope.addEmail = "";
+    angular.module('pokerPlanner').controller('groupCtrl', [
+        '$scope', '$stateParams', 'groupService',
+        function ($scope, $stateParams, groupService) {
+            const id = $stateParams.id;
+            $scope.addEmail = "";
 
-			$scope.addMember = function () {
-				groupService.addMember($scope.addEmail, $scope.group.id).then(response=>{
-					$scope.getGroup();
-				})
-				.catch(err=>{});
-			}
-			$scope.getGroup = function () {
-				groupService.getGroup(id).then(response=>{
-					$scope.group = response;
-				})
-				.catch(err=>{});
-			}
-			$scope.getGroup();
-			$scope.group = {};
-		}]);
+            $scope.addMember = function () {
+                groupService.addMember($scope.addEmail, $scope.group.id).then(response=>{
+                    $scope.getGroup();
+                })
+                .catch(err=>{});
+            }
+            $scope.getGroup = function () {
+                groupService.getGroup(id).then(response=>{
+                    $scope.group = response;
+                })
+                .catch(err=>{});
+            }
+            $scope.getGroup();
+            $scope.group = {};
+        }]);
 })();
