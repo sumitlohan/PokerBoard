69895567b82bbcecb92366a946343faa86c4a992
Prabhcharan <prabhcharan.sethi@joshtechnologygroup.com>
2021-08-26 14:07:16 +0530

    Updated docstring

--- modules/login/services.js
+++ modules/login/services.js
@@ -1,7 +1,7 @@
 'use strict';
 (function () {
     /**
-     * Returns details of user(first_name, last_name, id, email) and token(access, refresh))
+     * Returns details of user(first_name, last_name, id, email) and token
      */
     angular.module("pokerPlanner").service('loginService', [
         'Restangular', 'APP_CONSTANTS',
