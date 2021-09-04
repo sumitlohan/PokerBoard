3f06ac6916983f498646ff266cb849e538ea084a
siddhant792 <siddhantgupta792000@gmail.com>
2021-09-04 16:42:32 +0530

    Empty-field directive

--- directives/errorMsg/errorMsg.js
+++ directives/errorMsg/errorMsg.js
@@ -5,16 +5,16 @@
      */
     angular.module('pokerPlanner').directive('errorMsg', () => {
         var linker = (scope, el, attrs) => {
-            var ele = document.querySelector('[ng-model=' + attrs.inputField + ']');
+            var ele = document.querySelector('[ng-model=' + attrs.field + ']');
             ele.onfocus = () => {
-                scope.$watch("inputField", () => {
-                    scope.value = scope.inputField ? "" : ele.name + " is required";
+                scope.$watch("field", (extra) => {
+                    scope.value = scope.field ? "" : "Please enter a valid " + attrs.field;
                 });
             };
         };
         return {
             scope: {
-                inputField: '=inputField',
+                field: '=field',
             },
             templateUrl: 'directives/errorMsg/errorMsg.html',
             link: linker
