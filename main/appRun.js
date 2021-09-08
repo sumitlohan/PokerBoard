b68da5ab3c9c9efd86073667f1e7c876ca168830
Prabhcharan <prabhcharan.sethi@joshtechnologygroup.com>
2021-09-08 16:49:45 +0530

    Fixes

--- main/appRun.js
+++ main/appRun.js
@@ -6,7 +6,7 @@
         const token = user?.token;
         $rootScope.isAuth = token;
 
-        $rootScope.logout = () =>{
+        $rootScope.logout = () => {
             $cookies.remove("user");
             $rootScope.user = {};
             $rootScope.isAuth = false;
@@ -18,7 +18,9 @@
          */
         $transitions.onBefore({ to: "*" }, function (transition) {
             if (!APP_CONSTANTS.ROUTES.PUBLIC_ROUTES.find((route) => route === transition.to().name)) {
-                if ($rootScope.isAuth) {
+                const currentUser = JSON.parse($cookies.get('user') || ("{}"));
+                const authToken = currentUser?.token
+                if (authToken) {
                     if (APP_CONSTANTS.ROUTES.UNAUTH_ROUTES.find((route) => route === transition.to().name)) {
                         return transition.router.stateService.target('pokerboard');
                     }
